9df6033157f34289c74f1aeb50a51054e5ae3cab
Juan Ignacio Scarafia <juani.scarafia@gmail.com>
2020-12-16 18:59:05 -0300

    update project to react 16, install enzyme, enzyme-to-json, create first test

---
+++ src/tests/components/GifGridItem.test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import { GifGridItem } from '../../components/GifGridItem';
+
+describe('Pruebas en <GifGridItem />', () => {
+    const title = 'Un titulo';
+    const url = 'https://localhost/imagen.jpg';
+    const wrapper = shallow(<GifGridItem title={title} url={url}/>);
+
+    test('debe de mostrar el componente correctamente', () => {
+        expect(wrapper).toMatchSnapshot();
+    });
+
+    test('debe tener un parrafo con el title', () => {
+        const p = wrapper.find('p');
+        expect(p.text().trim()).toBe(title);
+    });
+
+    test('debe tener la imagen igual al url y title de los props', () => {
+        const img = wrapper.find('img');
+
+        expect(img.prop('src')).toBe(url);
+        expect(img.prop('alt')).toBe(title);
+    });
+    
+    test('debe tener animate__fadeIn', () => {
+        const div = wrapper.find('div');
+        const className = div.prop('className');
+
+        expect(className.includes('animate__fadeIn')).toBe(true);
+    });    
+    
+});
+
